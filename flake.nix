59710a79b589d7b880150c092ebec212b58be1ca
jzbor <jzbor@posteo.net>
2025-06-23 01:15:09 +0200

    Updating flake inputs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
 {
   description = "declarative attic user management";
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cf.url = "github:jzbor/cornflakes";
     cf.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, cf, crane }:
